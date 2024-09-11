32475e0f73dec5f7cdd14811f991e302eb6edded
Mehmet Enes Körhan (ETIYA) <enes.korhan@etiya.com>
2024-09-11 04:27:20 +0300

    signin and services structure have been refactored.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
-import {Button, TextField} from "@mui/material";
+import React, { useState } from 'react';
+import { Button, TextField } from "@mui/material";
 import axios from "axios";
 import * as HttpRequest from "../service/HttpRequest.jsx";
 
 function SignIn() {
+
+    const [token, setToken] = useState("");
+    const [success, setSucess] = useState(true);
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -12,12 +15,20 @@ function SignIn() {
         if (email.length == 0 || password.length == 0) {
             console.error("email or password cannot be null!")
         }
-        const req = {email, password};
-        console.log(req);
+        const req = { email, password };
 
-        const res = HttpRequest.post("http://localhost:8080/login", req);
-        console.log(res);
-        console.log(res.data);
+        const response = HttpRequest.post("login", req);
+
+        response.then(data => {
+            setToken(data.token);
+            setSucess(true);
+        }).catch(() => {
+            console.log("HATA");
+            (setSucess(false));
+        });
+
+        console.log("token: " + token);
+        console.log("success: " + success);
     }
 
     return (
@@ -40,6 +51,24 @@ function SignIn() {
             <Button variant="contained" color="primary" onClick={handleLogin}>
                 Login
             </Button>
+
+            <TextField
+                label="token"
+                type="token"
+                value={token}
+                onChange={(e) => setToken(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+
+            <TextField
+                label="success"
+                type="success"
+                value={success}
+                onChange={(e) => setSucess(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
         </div>
     )
 }
